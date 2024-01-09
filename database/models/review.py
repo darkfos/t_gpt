b160eb5a896ce868bd3dd3fb2005ce0c0d2a6a1c
fos <darkfos82@gmail.com>
2024-01-09 22:00:49 +0300

    Рефакторинг кода

--- database/models/review.py
+++ database/models/review.py
@@ -1,9 +1,10 @@
 from config import SQL_ALCHEMY_URL
+
 from sqlalchemy import BigInteger
 from sqlalchemy.orm import relationship, Mapped, mapped_column, DeclarativeBase
 from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine
 
-
+print(SQL_ALCHEMY_URL)
 #Логирование, подключение
 engine = create_async_engine(SQL_ALCHEMY_URL, echo=True)
 
@@ -20,3 +21,8 @@ class Review(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     tg_id: Mapped[int] = mapped_column(BigInteger)
     review_number: Mapped[int]
+
+
+async def create_db():
+    async with engine.begin() as connection:
+        await connection.run_sync(Base.metadata.create_all)
--- main.py
+++ main.py
@@ -1,5 +1,24 @@
 import asyncio
 import bot
+import logging
+import config
+
+from bot.handlers import router
+from aiogram import Dispatcher, Bot
+
+import database
+
+async def run_project():
+    await database.create_db()
+
+    #Бот
+    weather_bot = Bot(token=config.TG_API_KEY)
+    dp_w = Dispatcher(bot=weather_bot)
+    dp_w.include_router(router=router)
+
+    await dp_w.start_polling(weather_bot)
 
 if __name__ == "__main__":
-    asyncio.run(bot.main())
+    logging.basicConfig(level=logging.INFO)
+    asyncio.run(run_project())
+
