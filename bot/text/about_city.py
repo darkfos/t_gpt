cd83956631ece90540e425fb7bf6d0b462f303aa
fos <darkfos82@gmail.com>
2024-01-09 21:02:19 +0300

    Добавил текст к информации о городе

---
+++ bot/text/about_city.py
@@ -0,0 +1,9 @@
+
+about_of_city: list = [
+    ":ville: Тип поселения: ",
+    ":disque_noir: Регион: ",
+    ":disque_noir: Страна: ",
+    ":disque_noir: Тип локации: ",
+    ":disque_noir: Тайм зона: "
+]
+
