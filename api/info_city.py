9f5a5be12e135a107127940ddcbd06efb564b0af
fos <darkfos82@gmail.com>
2024-01-09 19:17:02 +0300

    Добавил функционал в метод api_get_info_of_city

--- api/info_city.py
+++ api/info_city.py
@@ -4,13 +4,25 @@ import config
 
 class City:
     def __init__(self, name_city: str):
-        self.__URL = ""
+        self.__URL = "http://dataservice.accuweather.com/locations/v1/cities/search"
         self.__API_KEY = config.API_KEY
         self.name_city = name_city
 
-    def api_get_info_of_city(self) -> dict:
+    def api_get_info_of_city(self, name_city: str) -> tuple:
+        parameters: dict = {
+            "apikey": self.__API_KEY,
+            "q": name_city,
+            "language": "ru"
+        }
         request = requests.get(url=self.__URL + self.name_city).json()
-        return dict()
+
+        type_colony: str = request.get("Type")
+        region: str = request["Region"]["LocalizedName"]
+        country: str = request["Country"]["LocalizedName"]
+        type_localization: str = request["AdministrativeArea"]["LocalizedType"]
+        time_zone: str = request["TimeZone"]["Name"]
+
+        return type_colony, region, country, type_localization, time_zone
 
     def __name__(self) -> str:
         return "Город: " + self.name_city
