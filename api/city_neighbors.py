2fef212ae85897388f9542f1a5fe773bf376bb3a
fos <darkfos82@gmail.com>
2024-01-10 14:53:43 +0300

    Добавил модель для получения ближайших городов

---
+++ api/city_neighbors.py
@@ -0,0 +1,25 @@
+import requests
+import config
+import api
+
+
+class CityNeighbors:
+    def __init__(self, name_city: str):
+        self.__URL = "http://dataservice.accuweather.com/locations/v1/cities/neighbors/"
+        self.__API_KEY = config.API_KEY
+        self.code_city = api.cities.get(name_city)[0]
+
+    def get_neighbors_city(self) -> list:
+        parameters = {
+            "api_key": self.__API_KEY,
+            "language": "ru"
+        }
+
+        request = requests.get(self.__URL + str(self.code_city), params=parameters).json()
+
+        all_neighbors_city: list = [city_name["LocalizedName"] for city_name in request]
+
+        return all_neighbors_city
+
+    def __repr__(self):
+        return "Код города: %s" % self.code_city
