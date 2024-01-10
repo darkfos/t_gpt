61d93c19938216362626cb41c5109ea3c99dea12
fos <darkfos82@gmail.com>
2024-01-10 16:03:00 +0300

    Фикс бага с apikey

--- api/city_neighbors.py
+++ api/city_neighbors.py
@@ -7,16 +7,15 @@ class CityNeighbors:
     def __init__(self, name_city: str):
         self.__URL = "http://dataservice.accuweather.com/locations/v1/cities/neighbors/"
         self.__API_KEY = config.API_KEY
-        self.code_city = api.cities.get(name_city)[0]
+        self.code_city: str = api.cities[name_city][0]
 
     def get_neighbors_city(self) -> list:
         parameters = {
-            "api_key": self.__API_KEY,
+            "apikey": self.__API_KEY,
             "language": "ru"
         }
 
-        request = requests.get(self.__URL + str(self.code_city), params=parameters).json()
-
+        request = requests.get(self.__URL + self.code_city, params=parameters).json()
         all_neighbors_city: list = [city_name["LocalizedName"] for city_name in request]
 
         return all_neighbors_city
--- bot/__init__.py
+++ bot/__init__.py
@@ -1,3 +1,3 @@
 from .text import text
 from .handlers import router
-from .keyboards import kb
+from .keyboards import kb, func_neighbors_cities
