e29ccd3f9d3d808f78c91d5fe8febe249c214a07
fos <darkfos82@gmail.com>
2024-01-10 13:11:49 +0300

    Обновил модель Review

--- database/models/review.py
+++ database/models/review.py
@@ -17,9 +17,13 @@ class Base(AsyncAttrs, DeclarativeBase):
 class Review(Base):
     __tablename__ = "review"
 
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     tg_id: Mapped[int] = mapped_column(BigInteger)
     review_number: Mapped[int]
+
+    def __init__(self, tg_id, review_number):
+        self.tg_id = tg_id
+        self.review_number = review_number
 
 
 async def create_db():
