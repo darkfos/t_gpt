93c0d8005170510b5cfcabc23651efc12a452e2f
fos <darkfos82@gmail.com>
2024-01-11 14:25:07 +0300

    Добавил в конфигурацию новый API

--- config.py
+++ config.py
@@ -4,5 +4,6 @@ config = ConfigParser()
 config.read("weather_configs.ini")
 
 API_KEY = config.get("auth", "API_KEY")
+API_KEY_GEO = config.get("auth", "API_KEY_GEO")
 TG_API_KEY = config.get("auth", "TG_API_KEY")
 SQL_ALCHEMY_URL = config.get("urls", "SQL_LITE_URL")
