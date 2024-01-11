7db72ff2ccf5549c98f44d5a85e4d342f75281e3
fos <darkfos82@gmail.com>
2024-01-11 15:59:58 +0300

    Добавил новый роутер, фикс багов

--- main.py
+++ main.py
@@ -3,19 +3,25 @@ import logging
 import config
 
 from bot.handlers import router
+from bot.quest_review import fsm_router
 from aiogram import Dispatcher, Bot
 
 import database
 
+
 async def run_project():
     await database.create_db()
 
-    #Бот
+    # Бот
     weather_bot = Bot(token=config.TG_API_KEY)
     dp_w = Dispatcher(bot=weather_bot)
-    dp_w.include_router(router=router)
+    dp_w.include_routers(
+        fsm_router,
+        router,
+    )
 
     await dp_w.start_polling(weather_bot)
+
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
