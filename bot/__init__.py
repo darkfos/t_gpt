9e83c6922ea324e2314e93fcef04cc0e8724ad96
fos <darkfos82@gmail.com>
2024-01-11 15:58:52 +0300

    Рефакторинг пакетов вызова

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,3 +1,5 @@
 from .text import text
 from .handlers import router
-from .keyboards import kb, func_neighbors_cities
+from .keyboards import kb, func_neighbors_cities, review_text_butt
+from .states import FormReview
+from .quest_review import fsm_router
--- bot/keyboards/__init__.py
+++ bot/keyboards/__init__.py
@@ -1,1 +1,1 @@
-from .kb import start_kb, func_about_city_kb, func_neighbors_cities
+from .kb import start_kb, func_about_city_kb, func_neighbors_cities, review_text_butt
