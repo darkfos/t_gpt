6e68cab8c83bcbb6b59340e8a92c9f4fcba18755
fos <darkfos82@gmail.com>
2024-01-12 22:05:10 +0300

    Фикс бага с выводом

--- bot/handlers/admin_handler.py
+++ bot/handlers/admin_handler.py
@@ -3,7 +3,7 @@ from aiogram.types import Message, CallbackQuery
 from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
 
-from bot import FormAdmin
+from bot import FormAdmin, get_admin_bt
 from database.services import admin_service
 
 
@@ -20,12 +20,26 @@ async def admin_command(message: Message, state=FSMContext):
     else:
         await message.answer("Для вас вход в админ панель запрещён")
 
+
 @admin_router.message(FormAdmin.password)
 async def password_admin(message: Message, state=FSMContext):
     password_admin: str = message.text
     response_to_admin_table = (await admin_service.get_one_admin(int(message.from_user.id)))[0][-1]
     if response_to_admin_table.password == password_admin:
         await message.answer("Успешно. Добро пожаловать в админ панель %s" % message.from_user.full_name)
+        await state.set_state(FormAdmin.sel_option)
+        await message.answer("Выберите пункт меню %s" % message.from_user.full_name, reply_markup=get_admin_bt())
     else:
         await message.answer("Пароль неверен")
 
+
+@admin_router.message()
+async def sel_option_admin(message: Message, state=FSMContext):
+    await state.clear()
+    match message.text:
+        case "Удалить отзыв":
+            await message.answer("Вы выбрали пункт меню удалить")
+        case "Узнать количество отзывов":
+            await message.answer("Вы выбрали пункт меню - количество отзывов")
+        case "Уникальный отзыв":
+            await message.answer("Вы выбрали пункт меню - уникальный отзыв")
