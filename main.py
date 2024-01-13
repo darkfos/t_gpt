f7ae85b4c8593d52f9c98fa037bafa7495b52d62
fos <darkfos82@gmail.com>
2024-01-13 20:01:00 +0300

    Обновлённый запуск

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import logging
 import config
 
 from bot.handlers import router
-from bot.handlers.quest_review import fsm_router
+from bot.handlers.callback_handler import fsm_router
 from bot.handlers import admin_router
 from aiogram import Dispatcher, Bot
 from database import create_db, admin_service
@@ -17,8 +17,9 @@ async def run_project():
     weather_bot = Bot(token=config.TG_API_KEY)
     dp_w = Dispatcher(bot=weather_bot)
     dp_w.include_routers(
-        fsm_router,
-        router, admin_router
+        router,
+        admin_router,
+        fsm_router
     )
     await dp_w.start_polling(weather_bot)
 
