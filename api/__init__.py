b714017a67b0326fd3e8037e3a61d92514c5a231
fos <darkfos82@gmail.com>
2024-01-14 16:19:39 +0300

    Добавил в пакет новые импорты

--- api/__init__.py
+++ api/__init__.py
@@ -1,4 +1,5 @@
 from .forecast_weather import Weather
 from .info_city import City
 from .city_neighbors import CityNeighbors
-from .code_cities import cities
+from .code_cities import cities, convert_fr_to_cl
+from .forecast_weather_5d import ForecastWeather5d
--- bot/keyboards/__init__.py
+++ bot/keyboards/__init__.py
@@ -1,2 +1,2 @@
-from .kb import start_kb, func_about_city_kb, func_neighbors_cities, review_text_butt
+from .kb import start_kb, func_about_city_kb, func_neighbors_cities, review_text_butt, kb_for_5d
 from .kb_admin import get_admin_bt
